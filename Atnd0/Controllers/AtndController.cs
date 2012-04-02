98c983aa46e4f135f88bd7886be40cfc6f9ab503
jsakamoto <junichi.jp@gmail.com>
2012-04-02 22:29:44 +0900

    first step.

---
+++ Atnd0/Controllers/AtndController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Atnd0.Models;
+
+namespace Atnd0.Controllers
+{
+    public class AtndController : Controller
+    {
+        private AtndDB db = new AtndDB();
+
+        // GET: /Atnd/
+        public ActionResult Index()
+        {
+            return View(db.Attendees.ToList());
+        }
+
+        // GET: /Atnd/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Atnd/Create
+        [HttpPost]
+        public ActionResult Create(Attendee attendee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Attendees.Add(attendee);
+                db.SaveChanges();
+                return RedirectToAction("Finish");
+            }
+
+            return View(attendee);
+        }
+
+        public ActionResult Finish()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Atnd0/Models/Attendee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Atnd0.Models
+{
+    public class Attendee
+    {
+        public int AttendeeID { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Email { get; set; }
+
+        [Required, MaxLength(20)]
+        public string TwitterID { get; set; }
+
+        public bool beParty { get; set; }
+    }
+}
